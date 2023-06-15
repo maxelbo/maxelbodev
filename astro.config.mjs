b6681d2214450afcbb951996b08e80f1d39d8926
Max Elbo <hello.maxelbo@gmail.com>
2023-06-16 06:20:53 +0900

    feat: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx(), partytown()]
+  integrations: [
+    tailwind(),
+    mdx({
+      optimize: true,
+    }),
+    partytown(),
+  ],
 });
