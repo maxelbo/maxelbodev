813eb6e5a93a7d7b9da9b4bcb535dcb1b322ab3e
Max Elbo <hello.maxelbo@gmail.com>
2023-08-22 05:29:49 +0900

    chore: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), partytown()],
+  integrations: [tailwind(), mdx(), partytown(), react()],
   experimental: {
     viewTransitions: true,
   },
