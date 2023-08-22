c8d5e04c7064828f9993e6a88dbe924c812e2504
Max Elbo <hello.maxelbo@gmail.com>
2023-08-23 06:01:30 +0900

    feat(404): add counter

---
+++ src/scripts/counter.js
@@ -0,0 +1,14 @@
+(() => {
+  let counter = document.getElementById("counter");
+  let from = parseInt(counter.getAttribute("data-from"));
+  let to = parseInt(counter.getAttribute("data-to"));
+  let speed = parseInt(counter.getAttribute("data-speed"));
+
+  let animation = setInterval(function () {
+    if (from <= to) {
+      counter.textContent = (from++).toString();
+    } else {
+      clearInterval(animation);
+    }
+  }, speed);
+})();
