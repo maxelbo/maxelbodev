005dcb2628a67a366277e558a4130f5d6e847d78
Max Elbo <hello.maxelbo@gmail.com>
2023-09-16 07:02:29 +0900

    chore(astro.config): upgrade to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), partytown(), react()],
-  experimental: {
-    viewTransitions: true,
-  },
+  integrations: [tailwind(), mdx(), react(), partytown()],
+  // experimental: {
+  //   viewTransitions: true,
+  // },
 });
