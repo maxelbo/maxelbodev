832c9c5a5f1390edb1e3fe574180248d4480be70
Max Elbo <hello.maxelbo@gmail.com>
2023-10-29 19:24:43 +0900

    feat(TagBox):use optional chaining in dekebabize

--- src/components/Tagbox.jsx
+++ src/components/Tagbox.jsx
@@ -1,13 +1,12 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
-import { dekebabize } from "@/scripts/tagActions";
+
+function dekebabize(prop) {
+  return prop?.split("-").join(" ");
+}
 
 export default function Tagbox({ tag, tagList }) {
   const [selected, setSelected] = useState(tag);
-
-  useEffect(() => {
-    setSelected(tag);
-  }, [tag]);
 
   return (
     <div className="w-full">
