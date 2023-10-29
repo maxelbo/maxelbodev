44fd233d834ee121e43ec58265eb2b301d4e91e3
Max Elbo <hello.maxelbo@gmail.com>
2023-10-29 19:17:07 +0900

    feat(TagBox):use effect

--- src/components/Tagbox.jsx
+++ src/components/Tagbox.jsx
@@ -1,9 +1,13 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { dekebabize } from "@/scripts/tagActions";
 
-export default function Tagbox({ tag = "all", tagList }) {
+export default function Tagbox({ tag, tagList }) {
   const [selected, setSelected] = useState(tag);
+
+  useEffect(() => {
+    setSelected(tag);
+  }, [tag]);
 
   return (
     <div className="w-full">
