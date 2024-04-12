e71904105a03de4c2db2be79d093eef662fd13f6
Max Elbo <hello.maxelbo@gmail.com>
2024-04-12 15:27:48 +0900

    feat(about):update profile data

--- src/data/profile.js
+++ src/data/profile.js
@@ -5,13 +5,14 @@ const profile = {
     siteName: "Elborium",
     title: "Max Elbo | Developer & Designer",
     description:
-      "Hi, I am Max Elbo, a digital craftsman, forging apps with the mighty hammers of CSS and JavaScript. Embark on a journey through my portfolio, where every pixel is meticulously chiseled into digital artifacts.",
+      "Hello, I am Max Elbo, a digital craftsman, forging apps with the mighty hammers of CSS and JavaScript. Embark on a journey through my portfolio, where every pixel is meticulously chiseled into digital artifacts.",
     keywords: [
       "max elbo",
       "front end developer, ui ux designer",
       "javascript, js node, react",
     ],
     author: "Max Elbo",
+    age: getAge(),
     role: "Designer / Developer",
     company: "Deloitte",
     city: "Tokyo",
@@ -36,19 +37,29 @@ const profile = {
       date: "2020 - present",
       place: "Tokyo, Japan",
       direction: "dir-r",
+      role: "UX Engineer",
       description:
         "I am currently working at Deloitte Consulting, doing frontend development. I've worked on corporate websites, and CRM apps to help the Japanese Government improve its DX.",
-      // clients: [
-      //   "Tokyo",
-      //   "Shibuya",
-      //   "Chiyoda",
-      //   "Nara Prefecture",
-      //   "Yokohama",
-      //   // "Daito Trust Construction",
-      //   // "Dunlop",
-      //   // "Toyota",
-      // ],
-      skills: ["Frontend Development", "Design", "Illustration"],
+      actions: [
+        "Designed, implemented, and refined the frontend of super-apps, CRM apps, and landing pages.",
+        "Bridged the gap between designers and engineers, ensuring the technical feasibility of designs prior to handoff, and implementing them in production according to the intended design vision.",
+        "Coded fully-functional prototypes directly in the framework used in production, developing reusable components and logic for production, significantly reducing the time from design to implementation.",
+      ],
+      clients: [
+        "Tokyo Bureau of Social Welfare",
+        "Shibuya Ward",
+        "Chiyoda Ward",
+        "Yokohama City",
+        "Nara Prefecture",
+      ],
+      skills: [
+        "Frontend Development",
+        "JavaScript",
+        "LWC",
+        "SASS",
+        "UI Design",
+        "Illustration",
+      ],
     },
     {
       company: "AMP",
@@ -57,17 +68,26 @@ const profile = {
       date: "2018 - 2020",
       place: "Tokyo, Japan",
       direction: "dir-l",
+      role: "Art Director/Illustrator",
       description:
         "I moved to Japan and worked at AMPlifier, a design studio in Tokyo. Here, I got to learn new skills, like motion graphics and 3D modeling.",
+      actions: [
+        "Designed websites for Japanese brands.",
+        "Created animations for museum exhibitions.",
+        "Crafted illustrations for advertising campaigns.",
+      ],
       clients: [
         "Tokyo National Museum",
         "KDDI Museum",
+        "Glico",
+        "Zojirushi",
+        "Mineo",
         "Dentsu Tec",
         "MullenLowe Dubai",
         "MullenLowe Japan",
         "VMLY&R France",
       ],
-      skills: ["Illustration", "Animation", "Web Design"],
+      skills: ["Web Design", "Animation", "Illustration"],
     },
     {
       company: "JWT Dubai",
@@ -76,8 +96,14 @@ const profile = {
       date: "2016 - 2017",
       place: "Dubai, UAE",
       direction: "dir-r",
+      role: "Art Director",
       description:
         "I moved to Dubai and worked as an Art Director at JWT (J. Walter Thompson, not JSON Web Token). I won some awards, including a Grand Prix at the Dubai Lynx.",
+      actions: [
+        "Conceptualized advertising campaigns.",
+        "Art directed television commercials and print ads.",
+        "Won local and international creative awards.",
+      ],
       clients: [
         "Coca-Cola",
         "Kenwood",
@@ -94,11 +120,18 @@ const profile = {
       date: "2013 - 2016",
       place: "Argentina - Mexico - Chile",
       direction: "dir-l",
+      role: "Illustrator",
       description:
         "I started working as an Illustrator, freelancing for publishing companies and advertising agencies, while living in different countries.",
+      actions: [
+        "Illustrated content for books and magazines.",
+        "Created illustrations for advertising campaigns and theater posters.",
+        "Storyboarded commercials for ad agencies.",
+      ],
       clients: [
-        "Editorial Confin",
-        "Fundación Ciencia Joven",
+        "Mirador Interactive Museum",
+        "Confin Ediciones",
+        "Ciencia Joven Foundation",
         "MullenLowe Dubai",
         "DDB Dubai",
         "JWT Dubai",
@@ -116,8 +149,14 @@ const profile = {
       date: "2012 - 2013",
       place: "Buenos Aires, Argentina",
       direction: "dir-r",
+      role: "Art Director",
       description:
         "I moved to BBDO as an Art Director, and had the chance to work with PepsiCo brands.",
+      actions: [
+        "Conceptualized advertising campaigns.",
+        "Art directed television commercials and print ads.",
+        "Won local creative awards.",
+      ],
       clients: ["Doritos", "7up", "Dr Lemon"],
       skills: ["Art Direction"],
     },
@@ -128,24 +167,48 @@ const profile = {
       date: "2011 - 2012",
       place: "Buenos Aires, Argentina",
       direction: "dir-l",
+      role: "Junior Art Director",
       description:
         "I started my career working as a Junior Art Director at Ogilvy & Mather Argentina. I did my first TV commercial here, and I won my first awards, including a silver lion at Cannes.",
+      actions: [
+        "Conceptualized advertising campaigns.",
+        "Art directed television commercials and print ads.",
+        "Won international creative awards.",
+      ],
       clients: ["Tang", "AMIA", "Mr. Muscle"],
       skills: ["Art Direction"],
     },
   ],
-  earlyLife: [
+  educationData: [
     {
+      logo: "snhu",
+      institution: "Southern New Hampshire University",
+      degree: "Bachelor of Science in Computer Science",
+      date: "2027 (expected)",
+    },
+    {
+      logo: "google",
+      institution: "Google",
+      degree: "Google UX Design Certificate",
+      date: "2023",
+    },
+    {
+      logo: "ztm",
+      institution: "Zero to Mastery Academy",
+      degree: "Fullstack Web Developer Bootcamp",
+      date: "2022",
+    },
+    {
+      logo: "jlpt",
+      institution: "JLPT",
+      degree: "Japanese-Language Proficiency Test N2",
+      date: "2021",
+    },
+    {
+      logo: "escuelita",
+      institution: "Escuela superior de creativos publicitarios",
+      degree: "Technical Diploma in Art Direction",
       date: "2011",
-      description: `I graduated from the Escuela superior de creativos publicitarios, a technical school in Buenos Aires, Argentina.`,
-    },
-    {
-      date: "2006",
-      description: `I graduated from the Grange School, an international school in Santiago, Chile.`,
-    },
-    {
-      date: "1988",
-      description: `I was born in Santiago, Chile, on July 25. That means that I'm ${getAge()} years old as you read this.`,
     },
   ],
   creativeAwards: [
@@ -216,13 +279,41 @@ const profile = {
     },
   ],
   exhibitions: [
-    "2016 - Art Stgo, GAM, Santiago, Chile",
-    "2016 - Solo Exhibition, Biblioteca Viva Sur, Santiago, Chile",
-    "2016 - De Radícibus—a show with my siblings, Biblioteca Viva Tobalaba, Santiago, Chile",
-    "2015 - Solo Exhibition, Biblioteca Viva Norte, Santiago, Chile",
-    "2015 - Café con Cuento, Galería Escaparate, Valparaíso, Chile",
-    "2010 - Solo Exhibition, Centro Cultural Belleasco, Buenos Aires, Argentina",
-    "2009 - Grand Small Works, juried art exhibition, F.U.E.L Collection Gallery, Philadelphia, PA",
+    {
+      date: "2016",
+      title: "Art Stgo",
+      location: "GAM, Santiago, Chile",
+    },
+    {
+      date: "2016",
+      title: "Solo Exhibition",
+      location: "Biblioteca Viva Sur, Santiago, Chile",
+    },
+    {
+      date: "2016",
+      title: "De Radícibus",
+      location: "Biblioteca Viva Tobalaba, Santiago, Chile",
+    },
+    {
+      date: "2015",
+      title: "Solo Exhibition",
+      location: "Biblioteca Viva Norte, Santiago, Chile",
+    },
+    {
+      date: "2015",
+      title: "Café con Cuento",
+      location: "Galería Escaparate, Valparaíso, Chile",
+    },
+    {
+      date: "2010",
+      title: "Solo Exhibition",
+      location: "Centro Cultural Belleasco, Buenos Aires, Argentina",
+    },
+    {
+      date: "2009",
+      title: "Grand Small Works",
+      location: "F.U.E.L Collection Gallery, Philadelphia, PA",
+    },
   ],
 };
 
