bc4dbe93b145d15a784a9daccee0faa70013d88c
Max Elbo <hello.maxelbo@gmail.com>
2024-05-27 06:21:00 +0900

    feat(about):increase transition duration

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,18 +2,18 @@ import { Disclosure, Transition } from "@headlessui/react";
 
 export default function Accordion({ title, children }) {
   return (
-    <Disclosure className="flex text-center justify-center">
+    <Disclosure className="flex justify-center text-center">
       {({ open }) => (
         <>
           <Disclosure.Button className="accordion-title relative w-0">
-            <h2 className="text-2xl font-bold flex justify-center">{title}</h2>
+            <h2 className="flex justify-center text-2xl font-bold">{title}</h2>
           </Disclosure.Button>
           <Transition
             show={open}
-            enter="transition duration-100 ease-out"
+            enter="transition duration-150 ease-out"
             enterFrom="transform scale-72 opacity-0"
             enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
+            leave="transition duration-100 ease-out"
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-72 opacity-0"
           >
