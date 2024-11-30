5072607cff3dcf47e0ace0fa87eb650470143a2c
Max Elbo <hello.maxelbo@gmail.com>
2024-11-30 19:36:12 +0900

    chore: backup in netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,19 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://maxelbo.dev",
   trailingSlash: "always",
-  integrations: [tailwind(), mdx(), react(), partytown()],
+
   // prefetch: { prefetchAll: true },
   // experimental: {
   //   viewTransitions: true,
   // },
+  integrations: [tailwind(), mdx(), react(), partytown()],
+
+  output: "server",
+  adapter: netlify(),
 });
