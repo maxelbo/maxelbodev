45c09807a3257d0acaf26769a9b85800644fbdea
Max Elbo <hello.maxelbo@gmail.com>
2026-03-24 08:37:29 +0900

    feat: start adding dark mode

--- src/components/Tagbox.jsx
+++ src/components/Tagbox.jsx
@@ -8,7 +8,7 @@ export default function Tagbox({ pageTitle, tagList }) {
   return (
     <Listbox value={pageTitle} className="relative mt-1">
       <div className="relative mt-1">
-        <Listbox.Button className="relative h-12 w-full cursor-default rounded-lg bg-white dark:bg-black py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+        <Listbox.Button className="relative h-12 w-full cursor-default rounded-lg bg-white dark:bg-slate-800 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
           <span className="capitalize">{dekebabize(pageTitle)}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <svg
@@ -34,13 +34,13 @@ export default function Tagbox({ pageTitle, tagList }) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-slate-800 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {tagList.map((tagOption, i) => (
               <Listbox.Option
                 key={i}
                 className={({ selected }) =>
                   `relative cursor-default select-none py-2 pl-3 text-left
-                    ${selected ? "bg-slate-100 text-gray-900 dark:bg-slate-900 dark:text-gray-100" : "text-gray-700 dark:text-gray-400"}
+                    ${selected ? "bg-slate-100 text-gray-900 dark:bg-slate-600 dark:text-gray-100" : "text-gray-700 dark:text-gray-200"}
                   `
                 }
                 value={dekebabize(tagOption)}
